7e0e891ba9d13a1118564c0fa3589ff16f9de34a
Lessllie Real <rlessllie@yahoo.com>
2019-10-30 05:32:11 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+class Person
+  def initialize(name)
+    @name= name
+  end 
+  def name
+    @name
+  end 
+  
+  def initialize (job)
+    @job= job
+  end 
+  def job
+    @job
+  end
+  def job= ()
+end
+
+beyonce= Person.new 
+beyonce.name= (beyonce)
+beyonce.name 
+
+beyonce=Job.new 
+beyonce.job= ("Singer")
+beyonce.job
+
+
+
+
+
